32b134490ed237238982a71d26c1dd26df8bde83
juharyimami <juharyimami2011@gmail.com>
2023-01-16 22:31:59 +0300

    challenge solved

--- data.sql
+++ data.sql
@@ -1,5 +1,10 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals (ID,NAME,date_of_birth,escape_attempts,neutered,weight_kg) 
+VALUES (1, 'Agumon', '2020-02-03',0, true,10.23);
+INSERT INTO animals (ID,NAME,date_of_birth,escape_attempts,neutered,weight_kg) 
+VALUES (2, 'Gabumon', '2018-11-15',02, true,8.0);
+INSERT INTO animals (ID,NAME,date_of_birth,escape_attempts,neutered,weight_kg) 
+VALUES (3, 'Pikachu', '2021-01-7',1, false,15.04);
+INSERT INTO animals (ID,NAME,date_of_birth,escape_attempts,neutered,weight_kg) 
+VALUES (4, 'Devmon', '2017-05-12',5, true,11);
--- queries.sql
+++ queries.sql
@@ -1,3 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
-
-SELECT * from animals WHERE name = 'Luna';
+SELECT * from animals;
+SELECT * from animals where name like '%mon';
+SELECT name from animals WHERE date_of_birth BETWEEN '2016-01-01' and '2019-01-01';
+SELECT name from animals where escape_attempts <3;
+SELECT date_of_birth from animals where name = 'Agumon' or  name = 'Pikachu';
+SELECT name, escape_attempts from animals where weight_kg >10;
+SELECT * from animals where neutered = true;
+SELECT * from animals where name !='Gabumon';
+SELECT * from animals where weight_kg between 10.4 and 17.3;
